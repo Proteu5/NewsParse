95a02cfd39689feeeb08c67c27595609c72d689d
Proteu5 <MichaelCohee@Live.Com>
2017-02-09 18:19:46 -0500

    Added Basic Clipboard System
    Embedded Clipboard Menu with RW capabilities and a quick jump to URL

--- rss_parse.py
+++ rss_parse.py
@@ -13,20 +13,53 @@ __status__ = "In Production"
 
 import feedparser
 import webbrowser
+import sys
+
 
 # Menu system!
 def menu():
 	strs = ("1. Reuters: World News\n"
         	"2. Reuters: Technology News\n"
         	"3. Reuters: Business News\n"
-        	"4. Open URL\n"
-        	"5. Exit : ")
+        	"4. Reuters: Egypt \n"
+        	"5. Open URL\n"
+        	"6. Clipboard\n"
+        	"7. Exit : ")
 	choice = input(strs)
 	return int(choice) 
+
+def menu_io():
+	strs_2 = ("1. Write\n"
+        	"2. Read\n"
+        	"3. URL\n"
+        	"4. Exit : ")
+	choice_io = input(strs_2)
+	return int(choice_io) 
 
 def url():
 	choice_2 = input()
 	webbrowser.open(choice_2)
+
+def clip():
+	while True:
+		choice_io = menu_io()
+		if choice_io == 1:
+			choice_3 = input()
+			f=open("links","a+")
+			f.write(choice_3+"\n")
+			f.close
+		elif choice_io == 2:
+			print ("Read")
+			f=open("links","r")
+			if f.mode == 'r':
+				contents =f.read()
+				print (contents)
+		elif choice_io == 3:
+			print ("Enter a URL: ")
+			url()	
+		elif choice_io == 4:
+			break
+
 
 while True:
     choice = menu()
@@ -46,9 +79,17 @@ while True:
       for post in d.entries:
 	      print (post.title+ ":"+post.link+"\n")
     elif choice == 4:
+      print ("Egypt Headlines") 
+      d=feedparser.parse('http://feeds.reuters.com/reuters/AFRICAegyptNews')
+      for post in d.entries:
+	      print (post.title+ ":"+post.link+"\n")
+    elif choice == 5:
     	print ("Enter a URL: ") 
     	url()
-    elif choice == 5:
+    elif choice == 6:
+    	print ("Clipboard: ")
+    	clip()
+    elif choice == 7:
         break
 
 
